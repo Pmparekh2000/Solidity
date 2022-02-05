e9b56fbf2dd9f86be2d39185aa8c61060db9d6a7
Pmparekh2000 <pmp.tech2000@gmail.com>
2022-02-05 20:01:12 +0530

    Compiled the contract and generated jason file

---
+++ Dapps/web/contracts/demo.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity 0.8.6;
+
+contract demo {
+    uint number;
+
+    function setter(uint _number) public {
+        number = _number;
+    }
+
+    function getter() public view returns(uint) {
+        return number;
+    }
+}
